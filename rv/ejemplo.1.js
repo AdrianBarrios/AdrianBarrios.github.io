32edbd8b9ceaee5d14e86b6eec3ff2142c657c82
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-03-09 11:11:07 -0600

    Update ejemplo.1.js

--- rv/ejemplo.1.js
+++ rv/ejemplo.1.js
@@ -1,25 +1,30 @@
+
+
 function setup(){
-var forma = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 });
-malla = new THREE.Mesh( forma, material );
-
-escena = new THREE.PerspectiveCAmera();
-camara.position.z = 5;
-
-renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
-document.body.appendChild( renderer.domElement );
+  var forma = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+  malla = new THREE.Mesh( forma, material );
+  
+  escena = new THREE.Scene();
+  escena.add(malla);
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 5;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
 }
 
 function loop(){
-requestAnimationFrame( loop);
+  requestAnimationFrame( loop );
+  malla.rotation.y += 0.01;
+  malla.rotation.x += 0.01;
+  
+  renderer.render( escena, camara );
+} 
 
-malla.rotation.x += 0.01;
-malla.rotation.y += 0.01;
-
-renderer.render( escena, camara);
-}
-
-var camara, escena, malla,renderer;
-setup ();
+var renderer, camara, escena, malla;
+setup();
 loop();
+
