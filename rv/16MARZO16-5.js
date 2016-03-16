4fad81698a40b3e144415b1b6d07884f0ccc0006
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-03-16 11:06:24 -0600

    Update 16MARZO16-5.js

--- rv/16MARZO16-5.js
+++ rv/16MARZO16-5.js
@@ -1,7 +1,7 @@
 function  setup(){
 THREE.ImageUtils.crossOrigin='';
 var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/planets/earth_atmos_2048.jpg');
-var material = new THREE.MeshphongMaterial({map: textura} );
+var material = new THREE.MeshPhongMaterial({map: textura} );
 var forma = new THREE.SphereGeometry(1);
 malla = new THREE.Mesh(forma, material);
 malla.rotation.z += 0.25;
@@ -16,7 +16,7 @@ escena.add(malla);
 escena.add(luzPuntual);
 
   camara = new THREE.PerspectiveCamera();
-  camara.position.z = 10;
+  camara.position.z = 5;
   
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
