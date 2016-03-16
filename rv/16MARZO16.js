e3578e82a8d8cf39761e01f0827b0afc00dab16f
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-03-16 10:30:56 -0600

    Update 16MARZO16.js

--- rv/16MARZO16.js
+++ rv/16MARZO16.js
@@ -1,6 +1,6 @@
 function  setup(){
 THREE.ImageUtils.crossOrigin='';
-var textura = THREE.Imageutils.loadTexture('http://threejs.org/example/textures/crate.gif');
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/example/textures/crate.gif');
 var material = new THREE.MeshBasicMaterial({map: textura} );
 var forma = new THREE.BoxGeometry(1, 1, 1);
 malla = new THREE.Mesh(forma, material);
