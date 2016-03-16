b9d6d0ffd2b197056bc6ac759d7cabc53734b936
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-03-16 11:14:21 -0600

    Update 16MARZO16-5.js

--- rv/16MARZO16-5.js
+++ rv/16MARZO16-5.js
@@ -4,7 +4,7 @@ var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures
 var material = new THREE.MeshPhongMaterial({map: textura} );
 var forma = new THREE.SphereGeometry(1);
 malla = new THREE.Mesh(forma, material);
-malla.rotation.y += 0.25;
+malla.rotation.z += 0.25;
 
 var luzPuntual = new THREE.PointLight(0xFFFFFF);
 luzPuntual.position.x=10;
@@ -26,7 +26,7 @@ escena.add(luzPuntual);
 function loop(){
   requestAnimationFrame( loop );
   malla.rotation.y += 0.01;
-  malla.rotation.x += 0.01;
+
   
   renderer.render( escena, camara );
 } 
