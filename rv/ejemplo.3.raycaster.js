0809ee88656141831ee9065f22ea0ebd97ff74b3
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-03-30 10:57:11 -0600

    Create ejemplo.3.raycaster.js

---
+++ rv/ejemplo.3.raycaster.js
@@ -0,0 +1,52 @@
+function  setup(){
+
+cubo1 = new THREE.Mesh(  new THREE.BoxGeometry(1,1,1),
+                         new THREE.MeshNormalMaterial());
+cubo2 = new THREE.Mesh(  new THREE.BoxGeometry(1,1,1),
+                         new THREE.MeshNormalMaterial());
+pelota = new THREE.Mesh(  new THREE.SphereGeometry(0.5),
+                         new THREE.MeshNormalMaterial());
+                         
+cubo1.position.x = 7;
+cubo2.position.x = -7;
+
+
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 20;
+  
+  raycaster1 = new THREE.Raycaster( pelota.position, new THREE.Vector3(1,0,0));
+  raycaster2 = new THREE.Raycaster( pelota.position, new THREE.Vector3(-1,0,0));
+
+  escena = new THREE.Scene();
+  escena.add(cubo1);
+  escena.add(cubo2);
+  escena.add(pelota);
+  escena.add(camara);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95,
+                    window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
+  step = 0.01;
+  
+}
+
+function loop(){
+  var intersects = raycaster.intersectObjects(escena.children);
+  if(intersects.length > 0) step = -step;
+  
+  cubo1.rotation.x += step;
+  cubo1.rotation.y += step;
+  cubo2.rotation.x += step;
+  cubo2.rotation.y += step;
+  
+  renderer.render( escena, camara );
+  requestAnimationFrame(loop);
+  
+  
+} 
+
+var cubo1, cubo2, escena, camara, renderer;
+var raycaster, step;
+
+setup();
+loop();
