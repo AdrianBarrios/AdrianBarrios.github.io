ccbe040c7f75bba1e6a6607d1152ad6a13fd32f0
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-04-01 19:39:54 -0600

    Update modelo.js

--- rv/modelo.js
+++ rv/modelo.js
@@ -23,7 +23,7 @@ var material = new THREE.MeshNormalMaterial();
 		
 		
 	mallaRueda1.position.set( 0, 0, 0);
-	mallaBase.position.set( 0, -10, 50);
+	mallaBase.position.set( 0, -5, 50);
 	mallaRueda2.position.set( 0, 0, 100);
 	mallaSoporte.position.set( 0, 0, -200);
 	mallaSoporte2.position.set( 50, 0, -250 );
