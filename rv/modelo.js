b170d5d33f6ddfe34138f50ed9305a14cccb6fca
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-04-01 20:02:40 -0600

    Update modelo.js

--- rv/modelo.js
+++ rv/modelo.js
@@ -21,7 +21,7 @@ var material = new THREE.MeshNormalMaterial();
 		var mallaSoporte = new THREE.Mesh( soporte);
 		var mallaSoporte2 = new THREE.Mesh( soporte2);
 		
-	mallaSoporte.rotation.x = (180)	
+	mallaSoporte.rotation.x = (120)	
 	mallaRueda1.position.set( 0, 0, 0);
 	mallaBase.position.set( 0, 0, 50);
 	mallaRueda2.position.set( 0, 0, 100);
