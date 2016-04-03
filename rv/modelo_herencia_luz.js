e332bda27748edfe7c94ac50053b1fde9f45d37c
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-04-03 12:44:28 -0500

    Update modelo_herencia_luz.js

--- rv/modelo_herencia_luz.js
+++ rv/modelo_herencia_luz.js
@@ -69,7 +69,7 @@ function setup(){
 	escena.add(luzPuntual);
 
 camara = new THREE.PerspectiveCamera();
-	camara.position.z = 500;
+	camara.position.z = 350;
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
