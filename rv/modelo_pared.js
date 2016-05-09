a4a458d5b490b59b778728212ebb3dfa21a0b29e
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-05-09 09:58:43 -0500

    Update modelo_pared.js

--- rv/modelo_pared.js
+++ rv/modelo_pared.js
@@ -62,7 +62,7 @@ function setup(){
   
   	camara = new THREE.PerspectiveCamera();
 	camara.position.y = 500;
-	camara.rotation.x = -1.57;
+	camara.rotation.x = -20;
 	raycaster1 = new THREE.Raycaster( mallaRueda1.position, new THREE.Vector3(1,0,0));
   	raycaster2 = new THREE.Raycaster( mallaRueda2.position, new THREE.Vector3(-1,0,0));	
   
