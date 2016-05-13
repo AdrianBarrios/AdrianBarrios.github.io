782ad6b17b81b009fbcab6e1a2dc2c19f55f0ad1
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-05-12 19:08:10 -0500

    Update modelo_pared.js

--- rv/modelo_pared.js
+++ rv/modelo_pared.js
@@ -34,10 +34,13 @@ base.prototype = new THREE.Object3D();
 function setup(){
 	var material = new THREE.MeshPhongMaterial({color: 0x0000ff });
 	
-		cubo1 = new THREE.Mesh(  new THREE.BoxGeometry(1,1,1),
-                         new THREE.MeshNormalMaterial());
-		cubo2 = new THREE.Mesh(  new THREE.BoxGeometry(1,1,1),
-                         new THREE.MeshNormalMaterial());
+	
+	
+	Pared1= new THREE.Mesh(new THREE.BoxGeometry(3,10,100),new THREE.MeshPhongMaterial({color: '#00ff00'}));
+	Pared2= new THREE.Mesh(new THREE.BoxGeometry(100,10,3),new THREE.MeshPhongMaterial({color: '#ff0000'}));
+	Pared3= new THREE.Mesh(new THREE.BoxGeometry(3,10,100),new THREE.MeshPhongMaterial({color: '#0000ff'}));	
+	Pared4= new THREE.Mesh(new THREE.BoxGeometry(100,10,3),new THREE.MeshPhongMaterial({color: '#aacc00'}));
+	
 
 		var soporte = new THREE.BoxGeometry( 10,10,70);
 		var soporte2 = new THREE.CylinderGeometry(5,5.25 );
@@ -55,54 +58,108 @@ function setup(){
 		mallaSoporte.position.set( 0, 100, 50);
 		mallaSoporte2.position.set( 0, 50, 53 );
 		
-	var luzPuntual = new THREE.PointLight( 0xffff00 );
-  	luzPuntual.position.x = -500;
-  	luzPuntual.position.y = -500;
-  	luzPuntual.position.z = 0;
+	var forma = new THREE.Geometry();
+	THREE.GeometryUtils.merge(Segway,mallaRueda1 );
+	THREE.GeometryUtils.merge(Segway,mallaRueda2 );
+	THREE.GeometryUtils.merge(Segway,mallaSoporte );
+	THREE.GeometryUtils.merge(Segway,mallaSoporte2 );	
+	THREE.GeometryUtils.merge(Segway,mallaBase );	
+		
+
+	var material = new THREE.MeshNormalMaterial();
+	Segway = new THREE.Mesh(forma, material);
+	
+	Pared1.position.x=50;
+	Pared2.position.z=50;
+	Pared3.position.x=-50;
+	Pared4.position.z=-50;	
+		
+	var luzfocal = new THREE.SpotLight( 0xffffff, 5, 200, 0.2 );
+  	luzfocal.position.x = -500;
+  	luzfocal.position.y = -500;
+  	luzfocal.position.z = 0;
   
   	camara = new THREE.PerspectiveCamera();
 	camara.position.y = 500;
 	camara.rotation.x = -1.57;
 	raycaster1 = new THREE.Raycaster( mallaRueda1.position, new THREE.Vector3(1,0,0));
-  	raycaster2 = new THREE.Raycaster( mallaRueda2.position, new THREE.Vector3(-1,0,0));	
-  	raycaster3 = new THREE.Raycaster( mallaSoporte.position, new THREE.Vector3(2,0,0));
   	
   	
   	
   	escena = new THREE.Scene(); 
-	escena.add(mallaRueda1);
-	escena.add(mallaRueda2);
-	escena.add( mallaBase);
-	escena.add(luzPuntual);
-	escena.add(mallaSoporte);
-	escena.add(mallaSoporte2);
+	escena.add(luzfocal);
 	escena.add(camara);
-	escena.add(cubo1);
-	escena.add(cubo2);
-	
-	
+	escena.add(Pared1);
+	escena.add(Pared2);
+	escena.add(Pared3);
+	escena.add(Pared4);
 	
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 	document.body.appendChild( renderer.domElement );
 	step = 15;
 }
-function loop(){
+function loop() {
 
- obstaculo1 = raycaster1.intersectObject(cubo1);
- obstaculo2 = raycaster2.intersectObject(cubo2);
-   if((obstaculo1.length> 0 && (obstaculo1[0].distance<= 0.5)) ||
-    (obstaculo2.length> 0 && (obstaculo2[0].distance<= 0.5)))
-  step = -step;
-
-  mallaRueda1.position.x += step;
-  mallaRueda2.position.x += step;
-  mallaSoporte.position.x += step;
-//  mallaSoporte2.position.x += step;
   
+  Obs1=raycaster.intersectObject(Pared1);
+  Obs2=raycaster.intersectObject(Pared2);
+  Obs3=raycaster.intersectObject(Pared3);
+  Obs4=raycaster.intersectObject(Pared4);
+  
+  if ((Obs1.length>0) && (Obs1[0].distance<=10)){
+    raycaster.set(Segway.position,new THREE.Vector3(0,0,1));
+	dir=2;
+	
+  }
+  
+  if ((Obs2.length>0) && (Obs2[0].distance<=10)){
+    raycaster.set(Segway.position,new THREE.Vector3(-1,0,0));
+	dir=3;
+  }
+ if ((Obs3.length>0) && (Obs3[0].distance<=10)){
+    raycaster.set(Segway.position,new THREE.Vector3(0,0,-1));
+	dir=4;
+  }
+  
+  if ((Obs4.length>0) && (Obs4[0].distance<=10)){
+    raycaster.set(Segway.position,new THREE.Vector3(1,0,0));
+	dir=1;
+  }
+
+
+if (dir==1){
+	Segway.position.x+=step;
+	Segway.rotation.y=1.57;
+	Segway.rotation.z=0;
+	 
+	 	 
+  }
+  else if(dir==2){
+	Segway.position.z+=step;
+	Segway.rotation.y=0;
+	Segway.rotation.z=0;
+	 
+  }
+  else if(dir==3){
+	Segway.position.x-=step;
+	Segway.rotation.y=-1.57;
+	Segway.rotation.z=0;
+  }
+  else if(dir==4){
+	Segway.position.z-=step;
+	Segway.rotation.y=3.14;
+	Segway.rotation.z=0;
+    
+	
+	}
+ 
   renderer.render(escena,camara);
   requestAnimationFrame(loop);
 }
+
+var escena, camara, renderer, Pared1, Pared2,Pared3,Pared4, wally;
+var Obs1,Obs2,Obs3,Obs4;
 var cubo1, cubo2, mallaRobot, camara, escena, renderer;
 var mallaRueda1, mallaRueda2, mallaBase, mallaSoporte, mallaSoporte2;
 var raycaster1,raycaster2, raycaster3, step;
