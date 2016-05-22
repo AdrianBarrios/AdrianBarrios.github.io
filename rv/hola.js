91a2e840f498328b996b195ad2d3841f2b306d0c
AdrianBarrios <Adrian_tazul4@hotmail.com>
2016-05-22 14:10:31 -0500

    Update hola.js

--- rv/hola.js
+++ rv/hola.js
@@ -18,7 +18,7 @@ this.arcShape = new THREE.Shape();
  	this.Base = new THREE.Mesh( new THREE.BoxGeometry( 50,20,98 ), new THREE.MeshNormalMaterial() );
 	this.Soporte1 = new THREE.BoxGeometry( 10,10,70);
 	this.Soporte2 = new THREE.CylinderGeometry(5,5.25 );
-
+		
 		 this.Rueda1.position.y= 0;
 		this.Rueda2.position.z = 100;
 		this.Soporte1.position.y = 100;
